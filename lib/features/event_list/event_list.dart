441dadd31cdccb91b6a62760ee28357a78fe1f3f
junsuPark <krpjs0508@gmail.com>
2025-02-27 21:43:01 +0900

    feat: ev list

---
+++ lib/features/event_list/event_list.dart
@@ -0,0 +1,3 @@
+library;
+
+export 'src/event_list_page.dart';
---
+++ lib/features/event_list/src/event_list_page.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+final class EventListPage extends StatefulWidget {
+  const EventListPage({super.key});
+
+  @override
+  State<EventListPage> createState() => _EventListPageState();
+}
+
+final class _EventListPageState extends State<EventListPage> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
