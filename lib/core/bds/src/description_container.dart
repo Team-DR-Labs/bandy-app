8c9c000bbe6b6c6d5109d73032bfeb026693ae8b
junsuPark <krpjs0508@gmail.com>
2025-02-27 23:12:11 +0900

    feat: description container pads

--- lib/core/bds/src/description_container.dart
+++ lib/core/bds/src/description_container.dart
@@ -5,7 +5,7 @@ final class DescriptionContainer extends StatelessWidget {
     super.key,
     required this.title,
     required this.description,
-    this.padding = const EdgeInsets.all(12.0),
+    this.padding = EdgeInsets.zero,
     this.spacing = 0.0,
   });
 
@@ -18,25 +18,28 @@ final class DescriptionContainer extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Column(
-      mainAxisSize: MainAxisSize.min,
-      crossAxisAlignment: CrossAxisAlignment.stretch,
-      children: [
-        DefaultTextStyle(
-          // TODO: NULL CHECK
-          style: Theme.of(context).textTheme.labelMedium!.copyWith(
-                color: Theme.of(context).colorScheme.onSurfaceVariant,
-              ),
-          child: title,
-        ),
-        SizedBox(height: spacing),
-        DefaultTextStyle(
-          style: Theme.of(context).textTheme.labelLarge!.copyWith(
-                color: Theme.of(context).colorScheme.onSurface,
-              ),
-          child: description,
-        ),
-      ],
+    return Padding(
+      padding: padding,
+      child: Column(
+        mainAxisSize: MainAxisSize.min,
+        crossAxisAlignment: CrossAxisAlignment.stretch,
+        children: [
+          DefaultTextStyle(
+            // TODO: NULL CHECK
+            style: Theme.of(context).textTheme.labelMedium!.copyWith(
+                  color: Theme.of(context).colorScheme.onSurfaceVariant,
+                ),
+            child: title,
+          ),
+          SizedBox(height: spacing),
+          DefaultTextStyle(
+            style: Theme.of(context).textTheme.labelLarge!.copyWith(
+                  color: Theme.of(context).colorScheme.onSurface,
+                ),
+            child: description,
+          ),
+        ],
+      ),
     );
   }
 }
