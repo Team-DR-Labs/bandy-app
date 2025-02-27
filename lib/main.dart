b5121d98bf8f8183ca7c1bb80a84e50d4c523829
junsuPark <krpjs0508@gmail.com>
2025-02-27 21:41:38 +0900

    feat: main routes

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,5 @@
-import 'package:bandy/features/event_details/event_details.dart';
 import 'package:flutter/material.dart';
+import 'package:go_router/go_router.dart';
 
 void main() {
   runApp(const MyApp());
@@ -10,19 +10,44 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Demo',
+    return MaterialApp.router(
       themeMode: ThemeMode.dark,
-      theme: ThemeData(
-        brightness: Brightness.light,
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+      theme: ThemeData.dark(
         useMaterial3: true,
       ),
-      darkTheme: ThemeData(
-        brightness: Brightness.dark,
-        useMaterial3: true,
-      ),
-      home: EventDetails(),
+      routerConfig: _bandyRoute(),
     );
   }
 }
+
+enum BandyRoutes {
+  home(path: "home"),
+  eventList(path: "event_list"),
+  eventDetails(path: "event_details"),
+  ;
+
+  const BandyRoutes({
+    required this.path,
+  });
+
+  final String path;
+}
+
+GoRouter _bandyRoute() => GoRouter(
+      routes: [
+        GoRoute(
+          path: BandyRoutes.home.path,
+          builder: (context, state) {
+            return HomePage();
+          },
+        ),
+        GoRoute(
+          path: BandyRoutes.eventList.path,
+          builder: (context, state) {},
+        ),
+        GoRoute(
+          path: BandyRoutes.eventDetails.path,
+          builder: (context, state) {},
+        ),
+      ],
+    );
