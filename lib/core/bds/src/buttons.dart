1dee81f33ca085d195519b4336544e3f3bf9ace8
junsuPark <krpjs0508@gmail.com>
2025-03-03 20:41:09 +0900

    feat: buttons file

---
+++ lib/core/bds/src/buttons.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+final class PlainButton extends StatelessWidget {
+  const PlainButton({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
