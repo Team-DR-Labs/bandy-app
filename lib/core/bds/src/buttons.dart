55785442b19f62536832a3f1f1f7c77865f355fc
junsuPark <krpjs0508@gmail.com>
2025-03-03 20:49:07 +0900

    feat: plain button

--- lib/core/bds/src/buttons.dart
+++ lib/core/bds/src/buttons.dart
@@ -1,10 +1,30 @@
 import 'package:flutter/material.dart';
 
 final class PlainButton extends StatelessWidget {
-  const PlainButton({super.key});
+  const PlainButton({
+    super.key,
+    this.padding = const EdgeInsets.all(16.0),
+    required this.child,
+  });
+
+  final EdgeInsetsGeometry padding;
+
+  final Widget child;
 
   @override
-  Widget build(BuildContext context) {
-    return const Placeholder();
-  }
+  Widget build(BuildContext context) => Container(
+        alignment: Alignment.center,
+        padding: padding,
+        decoration: BoxDecoration(
+          color: Colors.white,
+          borderRadius: BorderRadius.circular(100.0),
+        ),
+        child: DefaultTextStyle(
+          style: Theme.of(context).textTheme.bodyLarge!.copyWith(
+                fontWeight: FontWeight.bold,
+                color: Colors.black,
+              ),
+          child: child,
+        ),
+      );
 }
